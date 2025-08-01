ac8f6ae5ad51b31474f4334f9b3f6cf9118fb711
riva773 <teslaicaer@gmail.com>
2025-08-01 17:57:16 +0900

    商品一覧画面の仮実装を追加

---
+++ src/database/migrations/2025_07_25_201726_create_items_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->integer('price');
+            $table->string('description');
+            $table->string('image')->nullable();
+            $table->enum('condition',['良好','目立った傷や汚れなし','やや傷や汚れあり','状態が悪い']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('items');
+    }
+}
---
+++ src/resources/views/items/index.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('css/index.css') }}">
+    <title>items</title>
+</head>
+<body>
+    <div class="item-container">
+    @foreach($items as $item)
+        <div class=" {{ $item->id }} ">
+            <img src=" {{ $item->image }}" alt="商品画像" class="item-img">
+            <p class="item-name"> {{ $item->name }}</p>
+        </div>
+    @endforeach
+    </div>
+</body>
+</html>
